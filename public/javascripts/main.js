ca0d569b687de589ac411473d261035eb312ca9a
matt <matthew.hajec02@gmail.com>
2024-10-30 09:42:50 -0400

    added client-side chat code

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -41,14 +41,83 @@ function createBotMessageElement(message) {
     return containerElem;
 }
 
+// returns a promise that resolves to the bot's response as a string
+async function queryBot(message) {
+    const response = await fetch("/api/message", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ message })
+    });
+
+    // Check the status code
+    if (response.status !== 200) {
+        throw new Error(`Failed to query bot: Status ${response.status}`);
+    }
+
+    const responseJSON = await response.json();
+
+    if (!responseJSON.message) {
+        throw new Error("Failed to query bot: No message in response");
+    }
+
+    return responseJSON.message;
+}
+
+function constructBotMessage(response) {
+    let message = response.response_text;
+
+    if (response.hint_suggestion) {
+        message += `\n\n${response.hint_suggestion}`;
+    }
+
+    if (response.next_step_question) {
+        message += `\n\n${response.next_step_question}`;
+    }
+
+    return message;
+}
+
 async function main() {
     const chatContainerElem = document.getElementById("chat-container");
+    const chatFormElem = document.getElementById("chat-input-form");
+    const chatInputElem = document.getElementById("chat-input");
 
-    const botMsgElem = createBotMessageElement("Hello, human! How can I help you today?");
-    const userMsgElem = createUserMessageElement("I'd like to know more about your services.");
+    // Add a listener to the chat input form
+    chatFormElem.addEventListener("submit", async (e) => {
+        e.preventDefault();
 
-    chatContainerElem.appendChild(botMsgElem);
-    chatContainerElem.appendChild(userMsgElem);
+        // Get the message from the form (value of #chat-input), then clear the form
+        const message = chatInputElem.value;
+        chatInputElem.value = "";
+
+        // Add the user's message to the chat
+        const userMessageElem = createUserMessageElement(message);
+        chatContainerElem.appendChild(userMessageElem);
+
+        // Query the bot
+        let botResponse;
+        try {
+            botResponse = await queryBot(message);
+        } catch (e) {
+            console.error(e);
+            alert(`An error occurred while querying the bot. ${e.message}`);
+            return;
+        };
+
+        if (botResponse.bypass_attempt_detected) {
+            alert("A bypass attempt was detected. Your teacher will not be pleased.");
+        }
+
+        const botMessage = constructBotMessage(botResponse);
+
+        console.log(botMessage);
+
+        // Create the bot message element
+        const botMessageElem = createBotMessageElement(botMessage);
+        chatContainerElem.appendChild(botMessageElem);
+    });
 }
 
 // Run main if the route is "/"
